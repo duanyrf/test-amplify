af3d172ef25b4cffcd49db3f2c30adaae313026c
Duany Dreyton <duanyrf@gmail.com>
2024-10-09 19:17:15 -0300

    add Prisma ORM suporte

---
+++ prisma/migrations/20241009221519_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
